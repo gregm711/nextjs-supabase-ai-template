307a20d140bce2d140938448ab52a2b9d473d4d9
gregm711 <miller.7285osu@gmail.com>
2025-03-13 10:17:57 -0500

    Working

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import HeaderAuth from "@/components/header-auth";
 import HeroSection from "@/components/hero-section";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-
+import Features from "@/components/features-1";
+import ContentSection from "@/components/content-1";
+import FAQs from "@/components/faqs";
+import Testimonials from "@/components/testimonials";
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <Features />
+      <ContentSection />
+      <Testimonials />
+      <FAQs />
       <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
         <ThemeSwitcher />
       </footer>
